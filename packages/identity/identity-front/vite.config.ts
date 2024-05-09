d1d1e64af01652537075dd1fe52452998f061838
cincarnato <cristian.incarnato@sondeos.com.ar>
2024-05-09 00:38:10 -0300

    build: publish try 3

--- packages/identity/identity-front/vite.config.ts
+++ packages/identity/identity-front/vite.config.ts
@@ -1,24 +1,41 @@
-import { resolve } from "node:path";
-import { defineConfig } from 'vite'
+import {resolve} from "node:path";
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import dts from "vite-plugin-dts";
+import vuetify from "vite-plugin-vuetify";
+import {fileURLToPath} from "node:url";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), dts()],
-  build: {
-    lib: {
-      entry: resolve(__dirname, "src/index.ts"),
-      name: "@drax/identity-front",
-      fileName: "drax-identity-front",
+    plugins: [
+        vue(),
+        vuetify({
+            autoImport: true,
+        }),
+        dts()
+    ],
+    optimizeDeps: {
+        include: ["vuetify"],
     },
-    rollupOptions: {
-      external: ["vue"],
-      output: {
-        globals: {
-          vue: "Vue"
+    resolve: {
+        alias: {
+            "@": fileURLToPath(new URL("./src", import.meta.url)),
         },
-      },
     },
-  },
+    build: {
+        lib: {
+            entry: resolve(__dirname, "src/index.ts"),
+            name: "@drax/identity-front",
+            fileName: "drax-identity-front",
+        },
+
+        rollupOptions: {
+            external: ["vue"],
+            output: {
+                globals: {
+                    vue: "Vue"
+                },
+            },
+        },
+    },
 });
