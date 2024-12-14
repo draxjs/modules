2f13ed60a54a5e51e7d039d7d9f349339acbc3dc
cincarnato <cristian.incarnato@sondeos.com.ar>
2024-12-14 11:32:31 -0300

    feat: crud list expansion panels

--- packages/crud/crud-vue/src/components/CrudFormList.vue
+++ packages/crud/crud-vue/src/components/CrudFormList.vue
@@ -13,18 +13,24 @@ const {field} = defineProps({
   singleLine: {type: Boolean, default: false},
   clearable: {type: Boolean, default: true},
   density: {type: String as PropType<'comfortable' | 'compact' | 'default'>, default: 'default'},
-  variant: {type: String as PropType<'underlined' | 'outlined' | 'filled' | 'solo' | 'solo-inverted' | 'solo-filled' | 'plain'>, default: 'filled'},
+  variant: {
+    type: String as PropType<'underlined' | 'outlined' | 'filled' | 'solo' | 'solo-inverted' | 'solo-filled' | 'plain'>,
+    default: 'filled'
+  },
 })
 
 function newItem() {
-  return field.objectFields ? field.objectFields.reduce((acc, field) => ({...acc, [field.name]: field.default }), {}) : []
+  return field.objectFields ? field.objectFields.reduce((acc, field) => ({
+    ...acc,
+    [field.name]: field.default
+  }), {}) : []
 }
 
-function getField(key: string):IEntityCrudField|undefined {
+function getField(key: string): IEntityCrudField | undefined {
   return field.objectFields ? field.objectFields.find(field => field.name === key) : undefined;
 }
 
-function hasField(key: string):boolean {
+function hasField(key: string): boolean {
   return field.objectFields ? field.objectFields.some(field => field.name === key) : false;
 }
 
@@ -43,45 +49,50 @@ defineEmits(['updateValue'])
 <template>
   <v-card class="mt-3" variant="flat" border>
 
-    <v-card-title class="text-h5">{{field.label}}</v-card-title>
+    <v-card-title class="text-h5">{{ field.label }}</v-card-title>
     <v-card-text>
-      <v-row>
-        <v-col cols="12" v-for="(item,index) in valueModel" :key="index" >
-          <v-row dense align="center">
-            <v-col cols="11">
-              <template v-for="key in Object.keys(item as Record<string, any>)" :key="key">
-                <crud-form-field
-                    v-if="hasField(key)"
-                    :entity="entity"
-                    :field="getField(key)"
-                    v-model="(valueModel[index] as any)[key]"
-                    :readonly="readonly"
-                    :parentField="field.name"
-                    :index="index"
-                    :density="density"
-                    :variant="variant"
-                    :clearable="clearable"
-                    :hide-details="hideDetails"
-                    :single-line="singleLine"
-                    @updateValue="$emit('updateValue')"
-                />
-              </template>
+      <v-expansion-panels>
+        <v-expansion-panel v-for="(item,index) in valueModel" :key="index">
 
-            </v-col>
-            <v-col cols="1">
-              <v-btn v-if="!readonly" icon @click="removeItem(index)" small class="text-red text--darken-3">
+          <v-expansion-panel-title>
+            {{ index }} - {{valueModel[index][Object.keys(valueModel[index] as any)[0]]}}
+
+            <template v-slot:actions="{expanded}">
+              <v-icon>{{expanded ? "mdi-menu-down" : "mdi-menu-up"}}</v-icon>
+
+              <v-btn v-if="!readonly" variant="text" @click="removeItem(index)" density="compact"  class="text-red text--darken-3">
                 <v-icon>mdi-close</v-icon>
               </v-btn>
-            </v-col>
-          </v-row>
-          <v-divider></v-divider>
+            </template>
 
-        </v-col>
-        <v-btn  icon @click="addItem" class="text-blue text--darken-3">
+          </v-expansion-panel-title>
+
+          <v-expansion-panel-text>
+            <template v-for="key in Object.keys(item as Record<string, any>)" :key="key">
+              <crud-form-field
+                  v-if="hasField(key)"
+                  :entity="entity"
+                  :field="getField(key)"
+                  v-model="(valueModel[index] as any)[key]"
+                  :readonly="readonly"
+                  :parentField="field.name"
+                  :index="index"
+                  :density="density"
+                  :variant="variant"
+                  :clearable="clearable"
+                  :hide-details="hideDetails"
+                  :single-line="singleLine"
+                  @updateValue="$emit('updateValue')"
+              />
+            </template>
+          </v-expansion-panel-text>
+
+        </v-expansion-panel>
+        <v-btn icon @click="addItem" class="text-blue text--darken-3">
           <v-icon>mdi-plus</v-icon>
         </v-btn>
 
-      </v-row>
+      </v-expansion-panels>
     </v-card-text>
 
 
