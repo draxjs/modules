16642b041ee3c5cb1bf439bb19ad4439642c847a
bloome-alex <alex99valdiviezo@gmail.com>
2025-06-24 17:54:36 -0300

    feat(Crud:Filters): Se añade la posibilidad de customizar la sección filters sin necesidad de utilizar los filtros de la entidad (entity).

--- packages/crud/crud-vue/src/components/CrudList.vue
+++ packages/crud/crud-vue/src/components/CrudList.vue
@@ -109,10 +109,12 @@ defineEmits(['import', 'export', 'create', 'update', 'delete', 'view', 'edit'])
         </v-card-text>
 
         <v-card-text class="pt-0">
+          <slot name="filters"></slot>
           <crud-filters
-              :entity="entity"
-              v-model="filters"
-              @updateValue="doPaginate()"
+            v-if="!$slots.filters"
+            :entity="entity"
+            v-model="filters"
+            @updateValue="doPaginate()"
           />
         </v-card-text>
 
