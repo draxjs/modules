a9dbd7ccd0ae9ea59fd8a4e18ddd70a6219b9cf1
cincarnato <cristian.incarnato@sondeos.com.ar>
2025-06-30 17:21:35 -0300

    feat: se ajusta QueryFilterRegex para soportar - : .

--- packages/crud/crud-back/src/regexs/QueryFilterRegex.ts
+++ packages/crud/crud-back/src/regexs/QueryFilterRegex.ts
@@ -1,4 +1,4 @@
-const QueryFilterRegex = /^(?:[a-zA-Z0-9_]+;(?:eq|like|ne|in|nin|gt|gte|lt|lte);[a-zA-Z0-9_]+)(?:\|[a-zA-Z0-9_]+;(?:eq|like|ne|in|nin|gt|gte|lt|lte);[a-zA-Z0-9_]+)*$/
+const QueryFilterRegex = /^(?:[a-zA-Z0-9_\-]+;(?:eq|like|ne|in|nin|gt|gte|lt|lte);[a-zA-Z0-9_\-\:\.,]+)(?:\|[a-zA-Z0-9_\-]+;(?:eq|like|ne|in|nin|gt|gte|lt|lte);[a-zA-Z0-9_\-\:\.,]+)*$/
 
 export default QueryFilterRegex
 export {QueryFilterRegex}
