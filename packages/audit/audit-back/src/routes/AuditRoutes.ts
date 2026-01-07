80849c9336a922bada1954f4d7be423d1381755a
afurgeri <alejandro.furgeri@sondeosglobal.com>
2026-01-07 11:32:51 -0300

    feat(#160136): Missing export route path added in audit module.

--- packages/audit/audit-back/src/routes/AuditRoutes.ts
+++ packages/audit/audit-back/src/routes/AuditRoutes.ts
@@ -8,6 +8,8 @@ async function AuditFastifyRoutes(fastify, options) {
     const controller: AuditController = new AuditController()
     const schemas = new CrudSchemaBuilder(AuditSchema, AuditBaseSchema,AuditBaseSchema, 'Audit', 'openApi3', ['Audit']);
 
+    fastify.get('/api/audits/export', {schema: schemas.exportSchema}, (req, rep) => controller.export(req, rep))
+
     fastify.get('/api/audits', {schema: schemas.paginateSchema}, (req,rep) => controller.paginate(req,rep))
 
     fastify.get('/api/audits/find', {schema: schemas.findSchema}, (req,rep) => controller.find(req,rep))
@@ -17,8 +19,6 @@ async function AuditFastifyRoutes(fastify, options) {
     fastify.get('/api/audits/find-one', {schema: schemas.findOneSchema}, (req,rep) => controller.findOne(req,rep))
 
     fastify.get('/api/audits/group-by', {schema: schemas.groupBySchema}, (req,rep) => controller.groupBy(req,rep))
-
-
 }
 
 export default AuditFastifyRoutes;
